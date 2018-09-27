23e85f853197e2cdabdfa113d9cde0195b28de15
USER <USER@USER18-PC>
2018-09-27 14:13:14 +0900

    second push

---
+++ Exam10/src/kr/itedu/ex10/test/CalcTest.java
@@ -0,0 +1,16 @@
+package kr.itedu.ex10.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import kr.itedu.ex10.Calc;
+
+class CalcTest {
+
+	@Test
+	void test() {
+		assertEquals(3, Calc.sum(1,2));
+	}
+
+}
